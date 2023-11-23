b17885b4297416bc79f328f0766b569f9a15d8e9
Sisi Wang <sisiwang242@gmail.com>
2023-11-23 12:49:01 -0500

    update style

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Dog from "./assets/DataFlow"
 function App() {
 
   return (
-    <>
-    <Dog />
-    </>
+    <div className="min-w-screen min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-serif py-4 text-gray-100 font-bold">Course Practice</h1>
+      <Dog />
+    </div>
   )
 }
 
--- src/assets/DataFlow.jsx
+++ src/assets/DataFlow.jsx
@@ -1,10 +1,10 @@
 // Parent component: 
 function Dog() {
     return (
-        <div>
-            <h2 className="text-3xl text-blue-700 font-bold">Data Flow in React</h2>
+        <div className="min-w-screen flex flex-col text-center justify-center text-gray-100">
+            <h2 className="text-3xl text-blue-700">Data Flow in React</h2>
             <h3 className="text-2xl py-1">Parent-chile data flow</h3>
-            <p className="py-2 " style={{width: "500px"}}>A React app consists of many components, organized as a component tree. The data flows from the root component to all the components in the tree structure that require this data, using props.</p>
+            <p className="py-2 w-1/2 mx-auto">A React app consists of many components, organized as a component tree. The data flows from the root component to all the components in the tree structure that require this data, using props.</p>
             <p>Practice:</p>
             <Puppy name="Max" bowlShape="square" bowlStatus="full" />
         </div>

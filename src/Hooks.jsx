09cf1dee3cda9eb5615032bed52e1a181c6c46e0
Sisi Wang <sisiwang242@gmail.com>
2023-11-23 14:40:32 -0500

    update hooks practice

--- src/Hooks.jsx
+++ src/Hooks.jsx
@@ -12,7 +12,7 @@ export default function InputComponent() {
         <div className='text-gray-100 py-2 flex flex-col justify-center align-center' style={{lineHeight: "2.0"}}>
             <h2 className='text-blue-700' style={{textDecoration: 'underline'}}>Practice - Using Hooks</h2>
             <p>Happy Thanksgiving {name}</p>
-            <input className='block text-black px-2' value={name} onChange={handleNameChange} />
+            <input className='block bg-gray-200 text-black px-2' value={name} onChange={handleNameChange} />
             <button className='bg-blue-600 text-gray-200 rounded-md px-2 m-3 w-24 shadow-gray-600 shadow-md hover:bg-gray-200 hover:text-black' onClick={()=>setName("")}>
                 Reset
             </button>

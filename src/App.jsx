7aa56a0d1b10c26a1ac1898244239579f23103bb
Sisi Wang <sisiwang242@gmail.com>
2023-11-27 18:01:04 -0500

    add rounte

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import Calculator from "./calculator"
-import Dog from "./DataFlow"
+import DataFlow from "./DataFlow"
 import InputComponent from './Hooks'
 import MyVideoComponent from "./Video.jsx"
+import { BrowserRouter, Route, Routes, Link} from "react-router-dom";
 
 
 
 function App() {
 
   return (
-    <div className="min-w-screen min-h-screen gap-6 bg-gray-900 flex flex-col items-center justify-center">
+    <div className="w-screen h-screen gap-6 bg-gray-900 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-serif py-2 text-gray-100 font-bold">Course Practice</h1>
-      <Dog />
-      <InputComponent />
-      <MyVideoComponent />
-      <Calculator />
+      <BrowserRouter>
+      <nav className="text-gray-200 flex flex-row gap-10">
+        <Link to="/dataFlow">DataFlow</Link>
+        <Link to="/inputComponent">InputComponent</Link>
+        <Link to="/myVideoComponent">MyVideoComponent</Link>
+        <Link to="/calculator">Calculator</Link>
+      </nav>
+        <Routes>
+          <Route path='/dataFlow' element={<DataFlow />} />
+          <Route path='/inputComponent' element={<InputComponent />} />
+          <Route path='/myVideoComponent' element={<MyVideoComponent />} />
+          <Route path='/calculator' element={<Calculator />} />
+        </Routes>
+      </BrowserRouter>
     </div>
 
   )
 }
 
 
-export default App
+export default App;
--- src/DataFlow.jsx
+++ src/DataFlow.jsx
@@ -1,7 +1,7 @@
 // Parent component: 
-function Dog() {
+function DataFlow() {
     return (
-        <div className="min-w-screen flex flex-col text-center justify-center text-gray-100">
+        <div className="flex flex-col text-center text-gray-100">
             <h2 className="text-3xl text-blue-700">Data Flow in React</h2>
             <h3 className="text-2xl py-1">Parent-chile data flow</h3>
             <p className="py-2 w-1/2 mx-auto">A React app consists of many components, organized as a component tree. The data flows from the root component to all the components in the tree structure that require this data, using props.</p>
@@ -27,4 +27,4 @@ function Bowl(props) {
     );
 }
 
-export default Dog;
+export default DataFlow;
